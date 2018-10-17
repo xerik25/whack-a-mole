2bb6a08aa472c6967b8e84bb6bb2f084d9dbff86
Landon Cline <xerik25@gmail.com>
2018-10-17 01:32:04 -0700

    Setup timer for game

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,27 @@
 (function () {
     // Selectors
     const startBtn = document.querySelector('[data-start-btn]')
+    const timer = document.querySelector('[data-timer]')
+
+    // Variables
+    let score = 0
 
     // Methods
     const startGame = () => {
-        console.log('test')
+        let timeLeft = 30;
+        let timerId = setInterval(countdown, 1000);
+
+        function countdown() {
+            if (timeLeft == 0) {
+                clearTimeout(timerId);
+                timer.innerHTML = 'Game Over';
+            } else {
+                timer.innerHTML = timeLeft + ' seconds remaining';
+                timeLeft--;
+            }
+        }
     }
 
     // Event Listeners
     startBtn.addEventListener('click', startGame)
-})();
+})()
