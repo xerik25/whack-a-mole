b3edf461cb512cbdeb42b7ba1eede38a0af0ca13
Landon Cline <xerik25@gmail.com>
2018-10-17 23:13:12 -0700

    Adding methods and events to update score and stop the game

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,56 @@
 (function () {
     // Selectors
     const startBtn = document.querySelector('[data-start-btn]')
+    const stopBtn = document.querySelector('[data-stop-btn]')
     const timer = document.querySelector('[data-timer]')
+    const score = document.querySelector('[data-score]')
     const moles = document.querySelectorAll('[data-mole]')
 
     // Variables
-    let score = 0
+    let scoreCounter = 0
+    let timerId
+    let timeLeft = 5;
 
     // Methods
     const startGame = () => {
-        let timeLeft = 30;
-        let timerId = setInterval(countdown, 1000);
+        console.log('start')
+        timerId = setInterval(countdown, 1000)
+    }
 
-        function countdown() {
-            if (timeLeft == 0) {
-                clearTimeout(timerId);
-                timer.innerHTML = 'Game Over';
-            } else {
-                let mole = moles[Math.floor(Math.random()*moles.length)];
-                mole.style = 'transform: translate(0px, -67%);';
-                setTimeout(() => {
-                    mole.style = 'transform: translate(0px, -27%);';
-                }, 1000)
+    const stopGame = () => {
+        clearInterval(timerId)
+        scoreCounter = 0
+    }
 
-                timer.innerHTML = timeLeft + ' seconds remaining';
-                timeLeft--;
-            }
+    const updateScore = (mole) => {
+        mole.style = 'display: none;';
+        scoreCounter++
+        score.innerHTML = scoreCounter
+    }
+
+    function countdown() {
+        if (timeLeft == 0) {
+            clearTimeout(timerId);
+            timer.innerHTML = 'Game Over';
+        } else {
+            let mole = moles[Math.floor(Math.random() * moles.length)];
+            mole.style = 'transform: translate(0px, 37%);';
+            setTimeout(() => {
+                mole.style = 'transform: translate(0px, 77%);';
+            }, 1000)
+
+            console.log('timeLeft', timeLeft)
+            timer.innerHTML = timeLeft + ' seconds remaining';
+            timeLeft--;
         }
     }
 
     // Event Listeners
     startBtn.addEventListener('click', startGame)
+    stopBtn.addEventListener('click', stopGame)
+    for (var i = 0; i < moles.length; i++) {
+        ((index) => {
+            moles[index].addEventListener('click', () => updateScore(moles[index]))
+        })(i);
+    }
 })()
